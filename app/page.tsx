009080a84249912bd636f5e9cab60252eea5ee28
MELIFUT <camisetas.melipilla@gmail.com>
2026-09-03 19:17:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-export const revalidate = 60;
-
 import { client } from "@/lib/sanity";
 import Catalog from "@/components/Catalog";
 import { categoriesWithProductsQuery } from "@/lib/queries";
 
+export const revalidate = 60;
+
 export default async function Home() {
-  const categories = await client.fetch(categoriesWithProductsQuery);
+  const categories = await client.fetch(
+    categoriesWithProductsQuery,
+    {},
+    { next: { revalidate: 60 } }
+  );
 
   return (
     <div className="pt-[220px]">
